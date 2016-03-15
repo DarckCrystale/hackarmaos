7350f985bb5382c796d49c618f8833031a3fafb3
darckcrystale <crystale.darck@gmail.com>
2016-03-15 23:09:47 +0100

    Insertion de jQuery et remplacement de deux méthodes cassées.

---
+++ perso.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+    
+    /* ------------------------------------------------------------ */
+    /* Remplacement des vieilles fonctions qui plantent avec jQuery */
+    /* ------------------------------------------------------------ */
+    box_show_admin = function(i) {
+        $('#box_admin_'+i).css('display', 'block');
+    };
+    
+    box_hide_admin = function(i) {
+        $('#box_admin_'+i).css('display', 'none');
+    };
+    
+    
+    
+    
+    
+    
+});
