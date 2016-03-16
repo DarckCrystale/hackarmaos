6aa4bda69165249ea3f1047df0039ab29c0030e0
lucile-sticky <lucile@stickyads.tv>
2016-03-16 09:39:01 +0100

    Rajout d'un bloc pour éviter les conflits lors de l'appel de $() en cas de différentes library js utilisées.

--- perso.js
+++ perso.js
@@ -1,19 +1,23 @@
-$( document ).ready(function() {
-    
-    /* ------------------------------------------------------------ */
-    /* Remplacement des vieilles fonctions qui plantent avec jQuery */
-    /* ------------------------------------------------------------ */
-    box_show_admin = function(i) {
-        $('#box_admin_'+i).css('display', 'block');
-    };
-    
-    box_hide_admin = function(i) {
-        $('#box_admin_'+i).css('display', 'none');
-    };
-    
-    
-    
-    
-    
-    
-});
+// Evite les conflit en cas d'utilisation d'autres bibliothèques js
+// @voir http://codeimpossible.com/2010/01/13/solving-document-ready-is-not-a-function-and-other-problems/
+(function ($) {
+    $(document).ready(function () {
+
+        /* ------------------------------------------------------------ */
+        /* Remplacement des vieilles fonctions qui plantent avec jQuery */
+        /* ------------------------------------------------------------ */
+        box_show_admin = function (i) {
+            $('#box_admin_' + i).css('display', 'block');
+        };
+
+        box_hide_admin = function (i) {
+            $('#box_admin_' + i).css('display', 'none');
+        };
+
+
+
+
+
+
+    });
+})(jQuery);
